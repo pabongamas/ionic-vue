f6a7a4068531b01b34114ffe11798aae131b3636
jhon <pabongamas@gmail.com>
2023-11-22 18:01:02 -0500

    converted project to js

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,21 @@
+<template>
+   <ion-page>
+        <ion-header>
+            <ion-title>All memories</ion-title>
+        </ion-header>
+        <ion-content>
+            <h2>these are my memories - list to be added</h2>
+        </ion-content>
+   </ion-page>
+</template>
+<script>
+import {IonPage,IonHeader,IonTitle,IonContent} from '@ionic/vue'
+export default {
+    components:{
+        IonPage,
+        IonHeader,
+        IonTitle,
+        IonContent
+    }
+}
+</script>
